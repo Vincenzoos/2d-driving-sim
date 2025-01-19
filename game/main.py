9aaa8f588c1a338912b4f5869071577fe45c1c11
Vincenzoos <quocvietc18@gmail.com>
2025-01-19 19:45:55 +0700

    feat: set up NEAT neural network] - Set out essentials settings and config - Allow model to train self-driving car as expected

---
+++ game/main.py
@@ -0,0 +1,27 @@
+import neat
+import os
+from world import eval_genomes
+
+# setup NEAT neural network
+def run(config_path):
+    global pop
+    config = neat.Config(
+        neat.DefaultGenome,
+        neat.DefaultReproduction,
+        neat.DefaultSpeciesSet,
+        neat.DefaultStagnation,
+        config_path
+    )
+
+    pop = neat.Population(config)
+
+    pop.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    pop.add_reporter(stats)
+
+    pop.run(eval_genomes, 50)
+
+if __name__ == '__main__':
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, 'config.txt')
+    run(config_path)
