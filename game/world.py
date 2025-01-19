4629c4ecdfceff2040b622b7d55dc2503012da60
Vincenzoos <quocvietc18@gmail.com>
2025-01-19 11:02:35 +0700

    feat: rename main.py to word.py and add ai car - world.py will handle game environment - new main.py will handle neat algorithm setup and run the game

--- game/world.py
+++ game/world.py
@@ -1,6 +1,7 @@
 import pygame
 from utils import scale_img
 import car
+from car import mannualCar, autonomousCar
 import neat
 
 
@@ -32,15 +33,15 @@ pygame.display.set_caption("Driving Sim")
 FPS = 60
 clock = pygame.time.Clock()
 game_objects = [(GRASS, (0,0)), (TRACK, (0,0)), (FINISH_LINE, FINISH_LINE_POSITION), (TRACK_BORDER, (0,0))]
-player_car = car.Car(RED_CAR, 5, 5, 150, 250)
+player_car = mannualCar()
+ai_car = autonomousCar()
 
-# Game loop
-run = True
 
-def draw_objects(window: pygame.Surface, objects, car: car.Car):
+
+def draw_objects(window: pygame.Surface, objects):
     for obj, pos in objects:
         window.blit(obj, pos)
-    car.draw(window)
+    # car.draw(window)
     # Update the game each iteration
     pygame.display.update()
 
@@ -49,7 +50,10 @@ def eliminate(index):
     ge.pop(index)
     neural_nets.pop(index)
 
+# def eval_genomes():
 def eval_genomes(genomes, config):
+    # Game loop
+    run = True
     global cars, ge, neural_nets
     cars = []
     ge = []
@@ -69,7 +73,7 @@ def eval_genomes(genomes, config):
     while run:
         # Limit clock to 60 fps, ensure all machine run the game on the same speed
         clock.tick(FPS)
-        draw_objects(WINDOW, game_objects, player_car)
+        draw_objects(WINDOW, game_objects)
 
         # Event check
         for event in pygame.event.get():
@@ -85,7 +89,7 @@ def eval_genomes(genomes, config):
         # Update fitness point of each car, enliminate it from the game
         for i, car in enumerate(cars):
             ge[i].fitness += 1
-            if not car.sprite.alive:
+            if not car.is_alive():
                 eliminate(i)
 
         # Activate Steering action in autopilot mode for each car
@@ -101,27 +105,26 @@ def eval_genomes(genomes, config):
         # Update the state of car in autopilot mode for each car
         for car in cars:
             car.draw(WINDOW)
-            car.update_autopilot()
+            car.autonomous_drive()
 
         # Detect human control via key pressed
-        # player_car.update_mannual()
-        
-        
+        # player_car.mannual_drive() 
 
         # check for collision
-        if player_car.collide(TRACK_BORDER_MASK):
-            player_car.bounce()
+        # if player_car.collide(TRACK_BORDER_MASK):
+        #     player_car.bounce()
+        #     player_car.update_car_status(False)
         
-        finish_line_poi = player_car.collide(FINISH_LINE_MASK, *FINISH_LINE_POSITION)
-        if finish_line_poi:
-            if finish_line_poi[1] == 0:
-                player_car.bounce()
-            else:
-                print("You win!")
-                player_car.reset_position()
+        # finish_line_poi = player_car.collide(FINISH_LINE_MASK, *FINISH_LINE_POSITION)
+        # if finish_line_poi:
+        #     if finish_line_poi[1] == 0:
+        #         player_car.bounce()
+        #     else:
+        #         print("You win!")
+        #         player_car.reset_position()
 
         # draw radar
-        player_car.draw_radar(WINDOW, TRACK_BORDER_MASK)
+        # player_car.draw_radar(WINDOW, TRACK_BORDER_MASK)
         pygame.display.update()
 
     # Game termination
